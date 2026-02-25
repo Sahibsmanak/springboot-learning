2bc4d62ffda417788c6ef4648f252a40a401969d
Sahib Singh <100455785+Sahibsmanak@users.noreply.github.com>
2026-02-25 14:31:49 +0000

    Added how to add explicit packages for scanning

--- Spring-core-demo/Section1/src/main/java/com/springboot/dependencyInjection/DependencyInjectionApplication.java
+++ Spring-core-demo/Section1/src/main/java/com/springboot/dependencyInjection/DependencyInjectionApplication.java
@@ -3,7 +3,13 @@ package com.springboot.dependencyInjection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// If we need to scan any other package which is not present in the base package then that 
+// we need to add in the below format
+@SpringBootApplication (
+	scanBasePackages={"testing"}
+)
+
+
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
